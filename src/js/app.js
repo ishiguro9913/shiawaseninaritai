35873a9996fe6adf0ec186e8fa40b7766b2227d4
ishiguro tasuku <ishitasukuro9913@gmail.com>
2022-05-03 21:00:44 +0900

    Update:動くようになりました

--- src/js/app.js
+++ src/js/app.js
@@ -1,165 +1,63 @@
 require('../css/app.css');
 require('../scss/style.scss');
 
-import { init, Sprite, SpriteSheet, GameLoop, initKeys, keyPressed }  from "kontra";
+import { init, Sprite, SpriteSheet, GameLoop, initKeys, keyPressed } from "kontra";
 
 let { canvas } = init();
-initKeys();
 
-const maxUp = 25;
-const gravithY = 1;
-const jumpPower = 3;
+const image = new Image();
+image.src = 'images/test.png';
 
-const scoreTextBox = document.getElementById("score");
-const helpMessage = document.getElementById("help-message")
+const backimage = new Image();
+backimage.src = 'images/shiawase.png';
 
-let upScore = 0;
-let distance = 0;
+let sprite = Sprite({
+  image: image, 
+  x: 10,
+  y: 20,
+  color: "red",
+  width: 90,
+  height: 50,
+  dx: 2
+}); 
 
-let isOver = false;
-let loop;
+let button = Sprite({ 
+  image: backimage,
+  x: 100,
+  y: 20,
+  width: 100,
+  height: 100,
 
-function updateScore() {
-  scoreTextBox.innerHTML = Math.floor(distance);
-}
+});
 
-function jump(sprite) { 
-  upScore++;
+document.getElementById("text-button").onclick = function() {
+  document.getElementById("text").innerHTML = "ストップした！";
+  loop.stop();
 
-  if (upScore < maxUp) {
-    sprite.playAnimation("jump");
-    sprite.y -= jumpPower;
+  console.log(sprite.x)
+
+  if(90 < sprite.x && sprite.x < 120) {
+    document.getElementById("result").innerHTML = "すごい！"; 
+  } else {
+    document.getElementById("result").innerHTML = "だめだ！";  
   }
-}
-
-function stopJump(sprite) {
- upScore = maxUp;
-}
-
-function jumpCoolOff(sprite) {
-  sprite.playAnimation("walk");
-  upScore = 0;
-}
-
-function gravity(sprite) {
-  sprite.y += gravithY;
 };
 
-let block = Sprite({
-  x: canvas.width,
-  y: canvas.height - 50,
-  color: "#ff0",
-  width: 30,
-  height: 20,
-  dx: -3,
-  anchor: {x: 1, y: 1}
-});
+let loop = GameLoop({
+  update: function() {
+    sprite.update();
+    button.update();
 
-let ground = Sprite({
-  x: 0,
-  y: canvas.height - 50,
-  color: "#a0a0a0",
-  width: canvas.width,
-  height: 50
-});
-
-let image = new Image();
-image.src = "images/character_walk_sheet.png";
-
-image.onload = function() {
-
-  let spriteSheet = SpriteSheet({
-    image: image,
-    frameWidth: 72,
-    frameHeight: 97,
-    animations: {
-      walk: {
-        frames: "0..9",
-        frameRate: 30
-      },
-      jump: {
-        frames: 1,
-        frameRate: 1,
-      }
+    if(sprite.x > canvas.width) {
+      sprite.x = -sprite.width;
     }
 
-  });
+  },
+  render: function() {
+    sprite.render();
+    button.render();
 
-  let player = Sprite({
-    x: canvas.width / 2,
-    y: 305,
-    anchor: {x: 0.5, y: 0.5},
-
-    animations: spriteSheet.animations
-  });
-
-  loop = GameLoop({
-    update: function() {
-      if(keyPressed("up")) {
-        console.log("up");
-        jump(player);
-      } else { 
-        stopJump(player);
-      }
-
-      if(player.y < 305) {
-        gravity(player);
-
-      } else { 
-        jumpCoolOff(player);
-
-      }
-      player.update();
-      block.update();
-
-      if(block.x < 0) {
-        block.x = canvas.width;
-      }
-
-      if(player.collidesWith(block)) {
-        console.log("coll")
-        distance = 0;
-
-        isOver = true;
-
-        loop.stop();
-        helpMessage.innerHTML = "Restart: Press Enter Key";
-  
-      } else {
-        distance += 0.2;
-      }
-
-      updateScore();
-
-      if(Math.floor(distance) % 2 == 0) {
-        block.dx = -Math.random() * 6 - 3;
-      }
-
-      if(distance > 100) {
-        loop.stop();
-        helpMessage.innerHTML = "GAME CLEAR!! Restart: Press Enter Key"
-      }
-  
-    },
-    render: function() {
-      player.render();
-      ground.render();
-      block.render();
-    }
-  });
+  }
+});
 
 loop.start();
-};
-
-document.addEventListener("keyup", function(k) { 
-  if(isOver && k.code == "Enter") {
-    loop.start();
-    isOver = false;
-    console.log("press enter key");
-
-    block.x = canvas.width;
-    helpMessage.innerHTML = "";
-
-    distance = 0;
-  }
-});
